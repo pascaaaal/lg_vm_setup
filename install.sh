e11d1f43a72cafa8866fc286aa4498a624eeb4e8
pascaaal <pascal.faude@gmail.com>
2020-01-05 02:45:34 +0100

    Fixed bugs

--- install.sh
+++ install.sh
@@ -15,17 +15,17 @@ echo "Is this VM the Master? [y/n]: "
 read master
 if (("$master" == "y")); then
     ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-    sudo echo "4i; ViewSync settings\nViewSync/send = true\nViewSync/receive = false\nViewSync/hostname = $ip\nViewSync/port = 21567\nViewSync/yawOffset = 0\nViewSync/pitchOffset = 0.0\nViewSync/rollOffset = 0.0\nViewSync/horizFov = 36.5" > /opt/google/earth/pro/drivers.ini
+    sudo sed -i "4i; ViewSync settings\nViewSync/send = true\nViewSync/receive = false\nViewSync/hostname = $ip\nViewSync/port = 21567\nViewSync/yawOffset = 0\nViewSync/pitchOffset = 0.0\nViewSync/rollOffset = 0.0\nViewSync/horizFov = 36.5" > /opt/google/earth/pro/drivers.ini
 else
     echo "Please specifiy the yaw (Left: -36.5, Right: 36.5): "
     read yaw
-    sudo echo "4i; ViewSync settings\nViewSync/send = false\nViewSync/receive = true\nViewSync/port = 21567\nViewSync/yawOffset = $yaw\nViewSync/pitchOffset = 0.0\nViewSync/rollOffset = 0.0\nViewSync/horizFov = 36.5" > /opt/google/earth/pro/drivers.ini
+    sudo sed -i "4i; ViewSync settings\nViewSync/send = false\nViewSync/receive = true\nViewSync/port = 21567\nViewSync/yawOffset = $yaw\nViewSync/pitchOffset = 0.0\nViewSync/rollOffset = 0.0\nViewSync/horizFov = 36.5" > /opt/google/earth/pro/drivers.ini
 fi
 
 echo "Alright you're ready"
 echo "Do you want to start Google Earth? [y/n]"
-read run
+read runmrm
 if (("$run" == "y")); then
-    google-earth
+    google-earth-pro
 fi
 exit
